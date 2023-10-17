7ac0b55d1c05d92bc628ee13d8512574b5839adf
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-17 15:54:07 +0700

    push

---
+++ post/function.go
@@ -0,0 +1,29 @@
+package gcf
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/bursakerja/be_bursakerja/module"
+)
+
+func init() {
+	functions.HTTP("BursaKerja", bursaKerjaPost)
+}
+
+func bursaKerjaPost(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://bursakerja.github.io")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "https://bursakerja.github.io")
+	fmt.Fprintf(w, module.GCFPostHandler("PASETOPRIVATEKEY", "user", r))
+
+}
